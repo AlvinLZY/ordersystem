882cec022bbf8b18661883bd6c01cbf0f0d9d3a1
alexn <alexn@LAPTOP-HIAUVM72>
2022-03-25 00:03:20 +0800

    Title chanage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>LCK
+        <title>Zsasa
         </title>
         <link rel="stylesheet" href="{{URL::asset('css/app.css')}}">
     </head>
