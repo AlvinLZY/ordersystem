1eaf0d6848edc607dd6ac569d8aebf5a11abe255
alexn <alexn@DESKTOP-MTSF7BB>
2022-03-25 01:39:55 +0800

    Changed index page name

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Zsasa
+        <title>Xr
         </title>
         <link rel="stylesheet" href="{{URL::asset('css/app.css')}}">
     </head>
