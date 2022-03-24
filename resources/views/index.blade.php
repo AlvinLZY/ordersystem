4eda61e19ab339409d11d941e114bd6bcb52a18a
LokeCK <choonkeaiii@gmail.com>
2022-03-24 23:14:29 +0800

    CK pc 2nd

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Index Page</title>
+        <title>Alvin</title>
         <link rel="stylesheet" href="{{URL::asset('css/app.css')}}">
     </head>
     <body>
